76d09359a2cc6dcaf0e8145831c3f67d91df9c19
cekladous <cekladous@gmail.com>
2026-01-07 20:14:53 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,20 +1,20 @@
+import Capabilities from './pages/Capabilities';
 import Home from './pages/Home';
 import Portfolio from './pages/Portfolio';
-import Capabilities from './pages/Capabilities';
 import Process from './pages/Process';
-import About from './pages/About';
 import Contact from './pages/Contact';
+import About from './pages/About';
 import Admin from './pages/Admin';
 import __Layout from './Layout.jsx';
 
 
 export const PAGES = {
+    "Capabilities": Capabilities,
     "Home": Home,
     "Portfolio": Portfolio,
-    "Capabilities": Capabilities,
     "Process": Process,
-    "About": About,
     "Contact": Contact,
+    "About": About,
     "Admin": Admin,
 }
 
